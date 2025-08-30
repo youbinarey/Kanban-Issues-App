29ee28e69f1963abe68daaa62a2ae1fbc10952df
youbinarey <yeray.oubina.rey@gmail.com>
2025-08-30 17:33:34 +0200

    feat: add formatted createAt getter

--- lib/models/issue.dart
+++ lib/models/issue.dart
@@ -2,12 +2,15 @@ class Issue {
   final String id;
   String title;
   String description;
-  final DateTime createdAt;
+  final DateTime createdAt = DateTime.now();
 
   Issue({
     required this.id,
     required this.title,
     required this.description,
-    required this.createdAt,
+   
   });
+
+  String get createdAtString => createdAt.toLocal().toString().split('.').first;
+
 }
