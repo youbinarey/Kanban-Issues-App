5a0973d8f745f8669b21e3676857df7e3bf12de4
youbinarey <yeray.oubina.rey@gmail.com>
2025-08-31 19:24:40 +0200

    feat: add widget to display issue description

---
+++ lib/widgets/issues/issue_description.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:kanban_issues_app/models/issue.dart';
+
+class IssueDescriptionSection extends StatelessWidget {
+  const IssueDescriptionSection({
+    super.key,
+    required this.issue
+  });
+
+  final Issue issue;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          issue.title,
+          style: const TextStyle(fontSize: 16, height: 1.4),
+        ),
+        const SizedBox(height: 12),
+
+        Expanded(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(8),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(12),
+              child: Text(
+                issue.description,
+                style: const TextStyle(
+                  fontFamily: 'calibri',
+                  fontSize: 14,
+                  height: 1.4,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
